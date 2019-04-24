945adc51d720b4251c7af8b049b2bfc63802579f
cmlegault <chris.legault@noaa.gov>
2019-04-24 08:35:25 -0400

    add modified Sinclair Z function

--- inst/shiny_examples/SetUp/SinclairZ/app.R
+++ inst/shiny_examples/SetUp/SinclairZ/app.R
@@ -16,15 +16,60 @@ datdf <- data.frame()
 for (irow in 1:length(decoder[,1])){
   dat <- read.csv(paste0("..\\..\\..\\..\\ADIOS_data\\", decoder$ADIOS.name[irow], ".csv"))
   dat$decoder_row <- irow
+  dat$stock <- decoder$Short.Name[irow]
   datdf <- rbind(datdf, dat)
 }
 
-# tempo - replace with real Sinclair Z function
-Z = runif(30)
-szdf <- data.frame(Year = seq(1981, 2010),
-                  Z = Z,
-                  low90 = Z - 0.2,
-                  high90 = Z + 0.2)
+# modification of calc_Sinclair_Z function from ASAPplots to use data frame as input
+# mydf has columns Year, Age, cohort, lnVal and all lnVal=NA already removed
+get_Sinclair_Z_df <- function(mydf){
+  res <- list()
+
+  if (length(mydf$lnVal) == 0){
+    res$error <- TRUE
+    return(res)
+  }
+  
+  firstyear <- min(mydf$Year)
+  lastyear <- max(mydf$Year)
+  year <- seq(firstyear, lastyear)
+  ny <- lastyear - firstyear + 1
+
+  est.Sinclair.Z <- matrix(NA, nrow=(ny-3), ncol=3)
+  plot.year <- rep(NA, ny-3)
+  
+  for (i in 1:(ny-3)){
+    data <- filter(mydf, Year %in% seq(year[i], year[i + 3]))
+    can.calc <- FALSE
+    if (length(data[,1]) >= 2){
+      if (max(table(data$cohort)) >= 3){
+        can.calc <- TRUE
+      }
+    }
+    if (can.calc == TRUE){
+      my.lm <- lm(data$lnVal ~ as.factor(data$cohort) + data$Age)
+      data$pred <- predict(my.lm)
+      data$resid <- residuals(my.lm)
+      res[[i]] <- data
+      est.Sinclair.Z[i,1] <- -1 * my.lm$coefficients[names(my.lm$coefficients) == "data$Age"]
+      est.Sinclair.Z[i,2:3] <- -1 * rev(confint(my.lm, "data$Age", level=0.90))
+    }
+    else{  
+      res[[i]] <- data
+      est.Sinclair.Z[i,] <- rep(NA, 3)
+    }
+    plot.year[i] <- year[i] + 1.5
+  }
+  colnames(est.Sinclair.Z) <- c("Sinclair_Z","low90%","high90%")
+  res$est.Sinclair.Z <- est.Sinclair.Z
+  res$plot.year <- plot.year
+  res$Zests <- data.frame(Year = plot.year,
+                          Sinclair_Z = est.Sinclair.Z[,1],
+                          low90 = est.Sinclair.Z[,2],
+                          high90 = est.Sinclair.Z[,3])
+  res$error <- FALSE
+  return(res)
+}
 
 ###########################
 # Define UI for application
@@ -48,7 +93,7 @@ ui <- fluidPage(
                      "Age range for Z calcs:",
                      min = 0,
                      max = 50,
-                     value=c(4,10)),
+                     value=c(0,50)),
          
          # need to make dependent on input file
          checkboxInput("usesurvey",
@@ -59,7 +104,8 @@ ui <- fluidPage(
       # Show a plot of the generated distribution
       mainPanel(
         plotOutput("SinclairZ"),
-        tableOutput("sumtable")
+        tableOutput("sumtable"),
+        tableOutput("Zests")
       )
    )
 )
@@ -73,7 +119,14 @@ server <- function(input, output) {
   })
   
   mydat <- reactive({
-    filter(datdf, stock==input$stock, survey==input$survey)
+    filter(datdf, stock==input$stock, SURVEY==input$survey) %>%
+      filter(AGE %in% seq(input$ages[1], input$ages[2])) %>%
+      mutate(Year = YEAR, Age = AGE, cohort = YEAR - AGE, lnVal = log(NO_AT_AGE)) %>%
+      select(Year, Age, cohort, lnVal)
+  })
+  
+  res <- reactive({
+    get_Sinclair_Z_df(mydat())
   })
   
   surveyoption <- reactive({
@@ -88,7 +141,7 @@ server <- function(input, output) {
   })
   
   output$SinclairZ <- renderPlot({
-    ggplot(szdf, aes(x=Year, y=Z)) +
+    ggplot(res()$Zests, aes(x=Year, y=Sinclair_Z)) +
       geom_point() +
       geom_line() +
       geom_ribbon(aes(ymin=low90, ymax=high90), alpha = 0.3) +
@@ -96,7 +149,11 @@ server <- function(input, output) {
   })
   
   output$sumtable <- renderTable({
-    mydef()
+    head(mydat())
+  })
+  
+  output$Zests <- renderTable({
+    res()$est.Sinclair.Z
   })
 }
 
