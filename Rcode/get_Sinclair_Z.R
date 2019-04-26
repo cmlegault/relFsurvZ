3a43b893b50ed378ff2b69941c1873250a84646b
cmlegault <chris.legault@noaa.gov>
2019-04-26 14:01:28 -0400

    reorg to calc Sinclair Z only for selected combos

--- Rcode/get_Sinclair_Z.R
+++ Rcode/get_Sinclair_Z.R
@@ -8,64 +8,6 @@ library("ggplot2")
 decoder <- read.csv(".\\ADIOS_data\\file_decoder.csv")
 nstocks <- length(decoder$Short.Name)
 
-res <- list()
-resdf <- data.frame()
-for (istock in 1:nstocks){
-  dat <- read.csv(paste0(".\\ADIOS_data\\", decoder$ADIOS.name[istock], ".csv"))
-  surveys <- unique(dat$SURVEY)
-  nsurveys <- length(surveys)
-  res[[istock]] <- list()
-  res[[istock]]$stock <- decoder$Short.Name[istock]
-  res[[istock]]$surveys <- surveys
-  res[[istock]]$z <- list()
-  for (isurvey in 1:nsurveys){
-    sdat <- dat %>%
-      filter(SURVEY == surveys[isurvey])
-    minyear <- min(sdat$YEAR)
-    maxyear <- max(sdat$YEAR)
-    minage <- min(sdat$AGE)
-    maxage <- max(sdat$AGE)
-    mat <- matrix(NA, nrow = (maxyear - minyear + 1), ncol = (maxage - minage + 1), 
-                  dimnames = list(seq(minyear, maxyear), seq(minage, maxage)))
-    for (j in 1:length(sdat[,1])){
-      myrow <- sdat$YEAR[j] - minyear + 1
-      mycol <- sdat$AGE[j] - minage + 1
-      mat[myrow, mycol] <- sdat$NO_AT_AGE[j]
-    }
-    mysum <- apply(mat, 2, sum, na.rm=TRUE)
-    itest <- 1
-    while((mysum[itest + 1] - mysum[itest] > 0) & (itest < (maxage - minage + 1))){
-      mat[, itest] <- NA
-      itest <- itest + 1
-    }
-    myres <- calc_Sinclair_Z(mat)
-    res[[istock]]$z[[isurvey]] <- myres
-    if (myres$error == FALSE & !all(is.na(myres$est.Sinclair.Z))){
-      thisdf <- data.frame(stock = decoder$Short.Name[istock],
-                           survey = as.character(surveys[isurvey]),
-                           plotyear = rep(myres$plot.year, 3),
-                           Sinclair_Z = myres$est.Sinclair.Z[,1],
-                           low90 = myres$est.Sinclair.Z[,2],
-                           high90 = myres$est.Sinclair.Z[,3])
-      resdf <- rbind(resdf, thisdf)
-    }
-  }
-}
-resdf
-
-# first pass Sinclair Z plots
-for (istock in 1:nstocks){
-  thisstock <- decoder$Short.Name[istock]
-  p <- ggplot(filter(resdf, stock == thisstock), aes(x=plotyear, y=Sinclair_Z)) +
-    geom_point() +
-    geom_line() +
-    geom_ribbon(aes(ymin=low90, ymax=high90), alpha=0.3) +
-    facet_wrap(~survey) +
-    ggtitle(thisstock) +
-    theme_bw()
-  print(p)
-}
-
 # create default list of stocks, surveys, usesurvey (T/F), start age, end age, first year, last year
 defdf <- data.frame(stockID = integer(),
                     stock = character(),
@@ -78,7 +20,7 @@ defdf <- data.frame(stockID = integer(),
                     note = character())
 for (istock in 1:nstocks){
   dat <- read.csv(paste0(".\\ADIOS_data\\", decoder$ADIOS.name[istock], ".csv"))
-  surveys <- res[[istock]]$surveys
+  surveys <- unique(dat$SURVEY)
   nsurveys <- length(surveys)
   for (isurvey in 1:nsurveys){
     sdat <- dat %>%
@@ -88,7 +30,7 @@ for (istock in 1:nstocks){
     minage <- min(sdat$AGE)
     maxage <- max(sdat$AGE)
     thisdf <- data.frame(stockID = istock,
-                         stock = as.character(res[[istock]]$stock),
+                         stock = decoder$Short.Name[istock],
                          survey = as.character(surveys[isurvey]),
                          usesurvey = TRUE,
                          startage = minage,
@@ -102,10 +44,88 @@ for (istock in 1:nstocks){
 defdf$rowID <- seq(1, length(defdf[,1]))
 defdf
 write.csv(defdf, file=".\\ADIOS_data\\survey_options_orig.csv", row.names = FALSE)
-write.csv(defdf, file=".\\ADIOS_data\\survey_options_use.csv", row.names = FALSE)
 
+#############################################################################################
+# need to do some work by hand outside this program - described in this block of comments
+# save survey_options_use.csv only when run the first time
+# write.csv(defdf, file=".\\ADIOS_data\\survey_options_use.csv", row.names = FALSE)
 # now run Shiny app.R and change survey_options_use.csv for each stock/survey combination
-# note the file survey_options_use.csv will be overwritten - so be careful
+# note survey_options_use.csv will be overwritten if click Update button - so be careful
+# survey_options_use_backup.csv provided in case accidently overwrite the file
+# Shiny app located in directory ./inst/shiny_examples/SetUp/SinclairZ
+#############################################################################################
+
+# Estimate Sinclair Z based on age ranges for used surveys only and plot
+so_use <- read.csv(".\\ADIOS_data\\survey_options_use.csv", stringsAsFactors = FALSE)
+
+res <- list()
+resdf <- data.frame()
+for (istock in 1:nstocks){
+  dat <- read.csv(paste0(".\\ADIOS_data\\", decoder$ADIOS.name[istock], ".csv"))
+  surveys <- unique(dat$SURVEY)
+  nsurveys <- length(surveys)
+  res[[istock]] <- list()
+  res[[istock]]$stock <- decoder$Short.Name[istock]
+  res[[istock]]$surveys <- surveys
+  res[[istock]]$z <- list()
+  for (isurvey in 1:nsurveys){
+    myrow <- filter(so_use, stock == decoder$Short.Name[istock], survey == surveys[isurvey]) %>%
+      select(rowID) %>%
+      as.numeric(.)
+    if (so_use$usesurvey[myrow] == TRUE){
+      sdat <- dat %>%
+        filter(SURVEY == surveys[isurvey])
+      minyear <- min(sdat$YEAR)
+      maxyear <- max(sdat$YEAR)
+      minage <- min(sdat$AGE)
+      maxage <- max(sdat$AGE)
+      mat <- matrix(NA, nrow = (maxyear - minyear + 1), ncol = (maxage - minage + 1), 
+                    dimnames = list(seq(minyear, maxyear), seq(minage, maxage)))
+      for (j in 1:length(sdat[,1])){
+        myrow <- sdat$YEAR[j] - minyear + 1
+        mycol <- sdat$AGE[j] - minage + 1
+        mat[myrow, mycol] <- sdat$NO_AT_AGE[j]
+      }
+      mysum <- apply(mat, 2, sum, na.rm=TRUE)
+      itest <- 1
+      while((mysum[itest + 1] - mysum[itest] > 0) & (itest < (maxage - minage + 1))){
+        mat[, itest] <- NA
+        itest <- itest + 1
+      }
+      myres <- calc_Sinclair_Z(mat)
+      res[[istock]]$z[[isurvey]] <- myres
+      if (myres$error == FALSE & !all(is.na(myres$est.Sinclair.Z))){
+        thisdf <- data.frame(stock = decoder$Short.Name[istock],
+                             survey = as.character(surveys[isurvey]),
+                             plotyear = rep(myres$plot.year, 3),
+                             Sinclair_Z = myres$est.Sinclair.Z[,1],
+                             low90 = myres$est.Sinclair.Z[,2],
+                             high90 = myres$est.Sinclair.Z[,3])
+        resdf <- rbind(resdf, thisdf)
+      }
+    }
+  }
+}
+resdf
+write.csv(resdf, file=".\\figs\\resdf.csv", row.names = FALSE)
+
+# Sinclair Z plots
+pdf(file = ".\\figs\\Sinclair_Z_plots.pdf")
+for (istock in 1:nstocks){
+  thisstock <- decoder$Short.Name[istock]
+  p <- ggplot(filter(resdf, stock == thisstock), aes(x=plotyear, y=Sinclair_Z)) +
+    geom_point(na.rm = TRUE) +
+    geom_line(na.rm = TRUE) +
+    geom_ribbon(aes(ymin=low90, ymax=high90), alpha=0.3) +
+    facet_wrap(~survey) +
+    xlab("Year") +
+    ylab("Sinclair Z") +
+    ggtitle(thisstock) +
+    theme_bw()
+  print(p)
+}
+dev.off()
+
 
 
 
